afb7808a8c748f75922f47663a47310768e1bbaa
Patricio López <lopezjuripatricio@gmail.com>
2017-07-21 17:24:39 -0400

    Refactor session manager

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@
 
 "use strict";
 
-const bb = require("bot-brother");
-const redis = require("redis");
-const Bluebird = require("bluebird");
 const dedent = require("dedent");
 const _ = require("lodash");
 const moment = require("moment");
@@ -13,26 +10,20 @@ const SurBTC = require("@cryptolatam/surbtc").default;
 const CryptoMKT = require("@cryptolatam/cryptomkt").default;
 
 const createBot = require("./bot");
-
-Bluebird.promisifyAll(redis.RedisClient.prototype);
-Bluebird.promisifyAll(redis.Multi.prototype);
+const createSessionManager = require("./manager");
 
 const configuration = require("./configuration");
 const info = require("../package.json");
 
 const config = configuration();
 
+const manager = createSessionManager(config);
 const surbtc = new SurBTC({
   timeout: 5000,
 });
 const cryptomkt = new CryptoMKT({
   timeout: 5000,
 });
-const client = redis.createClient({
-  port: config.get("REDIS:PORT"),
-  host: config.get("REDIS:HOST"),
-});
-const manager = bb.sessionManager.redis({ client });
 
 // eslint-disable-next-line
 const bot = createBot({
